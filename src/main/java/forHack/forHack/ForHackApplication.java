7e865e3041f4f89ba1cf5ede5251bf470c2ec7fa
Dilnaza <dilnazbaidakhanova@gmail.com>
2025-10-16 17:31:57 +0500

    first commit

---
+++ src/main/java/forHack/forHack/ForHackApplication.java
@@ -0,0 +1,13 @@
+package forHack.forHack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForHackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/forHack/forHack/ForHackApplicationTests.java
@@ -0,0 +1,13 @@
+package forHack.forHack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForHackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
